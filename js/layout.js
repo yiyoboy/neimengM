3baea96b4828188c2a56477357222ace9ce55586
yiyoboy <18158626@qq.com>
2015-12-11 16:32:40 +0800

    修改首页

--- js/layout.js
+++ js/layout.js
@@ -1,1 +1,18 @@
-$(function(){})
+$(function(){
+    /* 倒计时跳转 */
+    countDown(3,'basic.html');
+
+})
+
+/* 倒计时跳转 */
+function countDown(secs,surl){
+    var jumpTo = document.getElementById('jumpTo');
+    jumpTo.innerHTML=secs;
+    if(--secs>0){
+        setTimeout("countDown("+secs+",'"+surl+"')",1000);
+    }
+    else{
+        location.href=surl;
+        -ma
+    }
+}
