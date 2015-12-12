d3688e7390aa54c0f35eece700029ef6d05e41cb
yiyoboy <18158626@qq.com>
2015-12-12 12:23:06 +0800

    tab切换

--- js/layout.js
+++ js/layout.js
@@ -1,13 +1,21 @@
 $(function(){
-    /* 倒计时跳转 */
-    countDown(3,'basic.html');
-
+    /* tab 切换 */
+   tabMenu(".topTab","active");
+   tabMenu(".menu-section-list","active");
 })
+/* menu 切换效果 */
+function tabMenu(className,activeName){
+    var target = $(className).find("li a");
+    touch.on(target, 'hold tap doubletap', function(ev){
+        $(this).addClass(activeName).parents("li").siblings("li").find("a").removeClass(activeName);
+    });
+}
+
 
 /* 倒计时跳转 */
 function countDown(secs,surl){
     var jumpTo = document.getElementById('jumpTo');
-    jumpTo.innerHTML=secs;
+    $(jumpTo).html(secs);
     if(--secs>0){
         setTimeout("countDown("+secs+",'"+surl+"')",1000);
     }
