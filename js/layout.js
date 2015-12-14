6c80cd654d377ce21c45246e3d63653482e31dd4
yiyoboy <18158626@qq.com>
2015-12-14 15:50:38 +0800

    table wenti

--- js/layout.js
+++ js/layout.js
@@ -3,7 +3,7 @@ $(function(){
    tabMenu(".topTab","active");
    tabMenu(".menu-section-list","active");
    tabMenu(".topNav","active");
-
+   toggleBox(".toggleBtn","open",".dateGuide",".picker__table")
 
 })
 /* menu 切换效果 */
@@ -14,6 +14,25 @@ function tabMenu(className,activeName){
     });
 }
 
+/* 收缩展开 */
+
+function toggleBox (btnName,activeName,parBox,hideBox){
+    $(btnName).click(function(){
+        var display = $(this).parents(parBox).find(hideBox).css("display");
+        console.log(display);
+        if(display=="block"){
+           $(this).addClass("open");
+            $(this).parents(parBox).find(hideBox).css("display","none");
+        }
+        else
+        {
+               $(this).removeClass(activeName);
+            $(this).parents(parBox).find(hideBox).css("display","block");
+
+        }
+
+    })
+}
 
 /* 倒计时跳转 */
 function countDown(secs,surl){
